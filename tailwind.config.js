e90282ca8e2317ff456bb135f3bc0f397f4badbc
Nan0-S <nan.fgbs@gmail.com>
2026-03-05 01:17:52 +0400

    feat: integrate TailwindCSS, GSAP, React Router and organize CSS structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+   "./index.hmtl",
+   "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+         
+      }
+    },
+  },
+  plugins: [],
+}
+

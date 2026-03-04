69c35ec8983a47215b995a961a5ae202f13436bf
Nan0-S <nan.fgbs@gmail.com>
2026-03-05 02:38:08 +0400

    style: extend color palette in Tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ function App() {
 
   return (
     <>
-      <div className="flex items-center justify-center w-full h-full px-9 py-6">
+      <div 
+         className="
+            flex justify-center 
+            w-full h-screen
+            px-9 py-6
+         "
+      >
          <h1 className="text-2xl">Home</h1>
       </div>
     </>
